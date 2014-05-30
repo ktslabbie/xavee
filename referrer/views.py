a1c4fae7297f1455eb1d81b9838ae6aff7e4545c
Kristian <kristian@agent-solve.com>
2014-05-30 16:44:22 +0900

    Empty index for app-install domain. Fix bugs?

--- referrer/views.py
+++ referrer/views.py
@@ -1,6 +1,11 @@
 from .models import Referral
 from django.shortcuts import render, get_object_or_404
 
+from django.views.generic import TemplateView
+
+class HomepageView(TemplateView):
+    template_name = "referrer_index.html"
+
 # Create your views here.
 def redirect(request, platform, app_name):
     referral = get_object_or_404(Referral, pk=request.GET.get('r'))
